25d6b72e5b6d5f262a3c1fa9ca7703b52cb5e828
u226 <me@gmail.com>
2024-08-03 14:11:42 +0330

    Dongle management fixed (PART2)

--- resources/views/NetworkEquipments/Dongles/edit.blade.php
+++ resources/views/NetworkEquipments/Dongles/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <main class="flex-1 bg-gray-100 py-6 px-8">
         <div class="mx-auto lg:mr-72">
-            <h1 class="text-2xl font-bold mb-4">جزئیات و ویرایش مودم</h1>
+            <h1 class="text-2xl font-bold mb-4">جزئیات و ویرایش دانگل</h1>
             @include('layouts.components.errors')
             <div class="bg-white rounded shadow flex flex-col ">
                 {{ html()->form('PATCH')->route('Dongles.update',$dongle->id)->acceptsFiles()->id('edit-catalog')->open() }}
@@ -31,7 +31,7 @@
                         <div>
                             <label for="type"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نوع
-                                مودم </label>
+                                دانگل </label>
                             <select name="type"
                                     class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     required>
@@ -67,11 +67,11 @@
                 </div>
 
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    @can('ویرایش مودم')
+                    @can('ویرایش دانگل')
                         <input type="hidden" name="id" value="{{ $dongle->id }}">
                         <button type="submit"
                                 class="px-4 py-2 mr-3 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-blue-300">
-                            ویرایش مودم
+                            ویرایش دانگل
                         </button>
                     @endcan
                     <button id="backward_page" type="button"
