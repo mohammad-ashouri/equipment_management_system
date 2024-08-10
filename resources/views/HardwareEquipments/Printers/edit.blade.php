1839718ceb85840f06996f741c1558afc839cd74
u226 <me@gmail.com>
2024-08-10 08:25:12 +0330

    Fix printer function type persian label in hardware equipments catalog

--- resources/views/HardwareEquipments/Printers/edit.blade.php
+++ resources/views/HardwareEquipments/Printers/edit.blade.php
@@ -31,7 +31,7 @@
                         <div>
                             <label for="function_type"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نوع
-                                اتصال </label>
+                                عملکرد </label>
                             <select name="function_type"
                                     class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     required>
