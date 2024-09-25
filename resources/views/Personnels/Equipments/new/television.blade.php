c1507b918eee2de114dc756b0919d977423100fc
u226 <me@gmail.com>
2024-09-25 13:08:17 +0330

    Store and update television fixed

---
+++ resources/views/Personnels/Equipments/new/television.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <label for="television"
+           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $equipmentType->persian_name }}</label>
+    <select name="television"
+            class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
+        <option value="" disabled selected>انتخاب کنید</option>
+        @foreach($televisions as $television)
+            <option value="{{ $television->id }}"
+                    @if(old('television')==$television->id) selected @endif>{{ $television->brandInfo->name}}
+                - {{ $television->model}} - {{ $television->size}} اینچ</option>
+        @endforeach
+    </select>
+</div>
