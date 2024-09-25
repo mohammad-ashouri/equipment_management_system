66c59533cae3ed8129a465ad3c7f9acc27963854
u226 <me@gmail.com>
2024-09-25 13:05:51 +0330

    Store and update camera holder fixed

---
+++ resources/views/Personnels/Equipments/edit/camera_holder.blade.php
@@ -0,0 +1,17 @@
+@php
+    $equipmentInfo=json_decode($equipment->info,true);
+@endphp
+<div>
+    <label for="camera_holder"
+           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $equipment->equipmentType->persian_name }} </label>
+    <select name="camera_holder"
+            class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
+        <option value="" disabled selected>انتخاب کنید</option>
+        @foreach($camera_holders as $camera_holder)
+            <option value="{{ $camera_holder->id }}"
+                    @if($equipmentInfo['camera_holder']==$camera_holder->id) selected @endif>{{ $camera_holder->brandInfo->name}}
+                - {{ $camera_holder->model}} - {{ $camera_holder->type}} - {{ $camera_holder->head_type}} - تعداد قطعات پایه:  {{ $camera_holder->parts_number}}</option>
+        @endforeach
+    </select>
+</div>
--- resources/views/Personnels/Equipments/new.blade.php
+++ resources/views/Personnels/Equipments/new.blade.php
@@ -1,4 +1,4 @@
-@php use App\Models\DigitalEquipments\ExternalHardDisk;use App\Models\DigitalEquipments\Mobile;use App\Models\DigitalEquipments\Phone;use App\Models\DigitalEquipments\Tablet;use App\Models\HardwareEquipments\Cases;use App\Models\HardwareEquipments\CopyMachine;use App\Models\HardwareEquipments\Cpu;use App\Models\HardwareEquipments\GraphicCard;use App\Models\HardwareEquipments\Headset;use App\Models\HardwareEquipments\InternalHardDisk;use App\Models\HardwareEquipments\Keyboard;use App\Models\HardwareEquipments\Monitor;use App\Models\HardwareEquipments\Motherboard;use App\Models\HardwareEquipments\Mouse;use App\Models\HardwareEquipments\Odd;use App\Models\HardwareEquipments\Power;use App\Models\HardwareEquipments\Printer;use App\Models\HardwareEquipments\Ram;use App\Models\HardwareEquipments\Scanner;use App\Models\HardwareEquipments\Voip;use App\Models\NetworkEquipments\Dongle;use App\Models\NetworkEquipments\Modem;use App\Models\NetworkEquipments\Rack;use App\Models\NetworkEquipments\Switches; @endphp
+@php use App\Models\DigitalEquipments\CameraHolder;use App\Models\DigitalEquipments\ExternalHardDisk;use App\Models\DigitalEquipments\Mobile;use App\Models\DigitalEquipments\Phone;use App\Models\DigitalEquipments\Tablet;use App\Models\HardwareEquipments\Cases;use App\Models\HardwareEquipments\CopyMachine;use App\Models\HardwareEquipments\Cpu;use App\Models\HardwareEquipments\GraphicCard;use App\Models\HardwareEquipments\Headset;use App\Models\HardwareEquipments\InternalHardDisk;use App\Models\HardwareEquipments\Keyboard;use App\Models\HardwareEquipments\Monitor;use App\Models\HardwareEquipments\Motherboard;use App\Models\HardwareEquipments\Mouse;use App\Models\HardwareEquipments\Odd;use App\Models\HardwareEquipments\Power;use App\Models\HardwareEquipments\Printer;use App\Models\HardwareEquipments\Ram;use App\Models\HardwareEquipments\Scanner;use App\Models\HardwareEquipments\Voip;use App\Models\NetworkEquipments\Dongle;use App\Models\NetworkEquipments\Modem;use App\Models\NetworkEquipments\Rack;use App\Models\NetworkEquipments\Switches; @endphp
 @extends('layouts.PanelMaster')
 @section('content')
     @switch($equipmentType->name)
@@ -92,6 +92,11 @@
         @case('mobile')
             @php
                 $mobiles=Mobile::get();
+            @endphp
+            @break
+        @case('camera_holder')
+            @php
+                $camera_holders=CameraHolder::get();
             @endphp
             @break
     @endswitch
