78315a01c3f93e87c35d3cfbca622a92f4399800
u226 <me@gmail.com>
2024-09-25 12:54:13 +0330

    Store and update mobile fixed

--- resources/views/Personnels/Equipments/new.blade.php
+++ resources/views/Personnels/Equipments/new.blade.php
@@ -1,4 +1,4 @@
-@php use App\Models\DigitalEquipments\ExternalHardDisk;use App\Models\DigitalEquipments\Phone;use App\Models\DigitalEquipments\Tablet;use App\Models\HardwareEquipments\Cases;use App\Models\HardwareEquipments\CopyMachine;use App\Models\HardwareEquipments\Cpu;use App\Models\HardwareEquipments\GraphicCard;use App\Models\HardwareEquipments\Headset;use App\Models\HardwareEquipments\InternalHardDisk;use App\Models\HardwareEquipments\Keyboard;use App\Models\HardwareEquipments\Monitor;use App\Models\HardwareEquipments\Motherboard;use App\Models\HardwareEquipments\Mouse;use App\Models\HardwareEquipments\Odd;use App\Models\HardwareEquipments\Power;use App\Models\HardwareEquipments\Printer;use App\Models\HardwareEquipments\Ram;use App\Models\HardwareEquipments\Scanner;use App\Models\HardwareEquipments\Voip;use App\Models\NetworkEquipments\Dongle;use App\Models\NetworkEquipments\Modem;use App\Models\NetworkEquipments\Rack;use App\Models\NetworkEquipments\Switches; @endphp
+@php use App\Models\DigitalEquipments\ExternalHardDisk;use App\Models\DigitalEquipments\Mobile;use App\Models\DigitalEquipments\Phone;use App\Models\DigitalEquipments\Tablet;use App\Models\HardwareEquipments\Cases;use App\Models\HardwareEquipments\CopyMachine;use App\Models\HardwareEquipments\Cpu;use App\Models\HardwareEquipments\GraphicCard;use App\Models\HardwareEquipments\Headset;use App\Models\HardwareEquipments\InternalHardDisk;use App\Models\HardwareEquipments\Keyboard;use App\Models\HardwareEquipments\Monitor;use App\Models\HardwareEquipments\Motherboard;use App\Models\HardwareEquipments\Mouse;use App\Models\HardwareEquipments\Odd;use App\Models\HardwareEquipments\Power;use App\Models\HardwareEquipments\Printer;use App\Models\HardwareEquipments\Ram;use App\Models\HardwareEquipments\Scanner;use App\Models\HardwareEquipments\Voip;use App\Models\NetworkEquipments\Dongle;use App\Models\NetworkEquipments\Modem;use App\Models\NetworkEquipments\Rack;use App\Models\NetworkEquipments\Switches; @endphp
 @extends('layouts.PanelMaster')
 @section('content')
     @switch($equipmentType->name)
@@ -87,6 +87,11 @@
         @case('phone')
             @php
                 $phones=Phone::whereStatus(1)->get();
+            @endphp
+            @break
+        @case('mobile')
+            @php
+                $mobiles=Mobile::whereStatus(1)->get();
             @endphp
             @break
     @endswitch
