24d11e06c86346eb90fab95ad26408ed924aca51
u226 <me@gmail.com>
2024-12-06 15:31:42 +0330

    Change model of whiteboards data

--- resources/views/TechnicalFacilities/Whiteboards/edit.blade.php
+++ resources/views/TechnicalFacilities/Whiteboards/edit.blade.php
@@ -24,9 +24,17 @@
                         <div>
                             <label for="model"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">مدل </label>
-                            <input type="text" name="model" value="{{ $whiteboard->model }}"
-                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="" required>
+                            <select name="model"
+                                    class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required>
+                                <option value="" disabled selected>انتخاب کنید</option>
+                                <option value="پایه دار" @if($whiteboard->model=='پایه دار') selected @endif>
+                                    پایه دار
+                                </option>
+                                <option value="دیواری" @if($whiteboard->model=='دیواری') selected @endif>
+                                    دیواری
+                                </option>
+                            </select>
                         </div>
                         <div>
                             <label for="material"
