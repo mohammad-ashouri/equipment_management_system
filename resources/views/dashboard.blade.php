eda9c647db44ae8e4f8adeb309ccf35bb707a196
u226 <me@gmail.com>
2024-12-07 08:46:39 +0330

    Add app persian name env to welcome view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <h1 class="text-2xl font-bold mb-4">داشبورد</h1>
             <div class="bg-white rounded shadow p-6">
                 <p>
-                    به پنل داشبورد خوش آمدید.
+                    به پنل {{ env('APP_PERSIAN_NAME') }} خوش آمدید.
                 </p>
             </div>
         </div>
